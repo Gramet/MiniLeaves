4002158a7a92975ae59ca903661ec7e1b508abab
Gaétan Ramet <gr@visium.ch>
2020-05-06 14:14:50 +0200

    added CNN notebook

---
+++ start.sh
@@ -0,0 +1,5 @@
+#Donwload the datasets
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/minileaves/v0.1/train-images.npy
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/minileaves/v0.1/train-labels.npy
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/minileaves/v0.1/test-images.npy
+wget https://s3.eu-central-1.wasabisys.com/aicrowd-practice-challenges/public/minileaves/v0.1/all_classes.txt
